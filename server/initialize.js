1c6279a1fa607aa41ab251f00c7db5146825dc46
Theodor Diaconu <diaconu.theodor@gmail.com>
2018-08-29 11:48:48 +0300

    Solved some issues with accounts and ssr

--- server/initialize.js
+++ server/initialize.js
@@ -72,6 +72,10 @@ export default function initialize(apolloConfig = {}, meteorApolloConfig = {}) {
       res.end();
     }
   });
+
+  return {
+    server,
+  };
 }
 
 function getContextCreator(meteorApolloConfig, defaultContextResolver) {
@@ -90,7 +94,9 @@ function getContextCreator(meteorApolloConfig, defaultContextResolver) {
     } else {
       let userContext = {};
       if (Package['accounts-base']) {
-        const loginToken = req.headers['meteor-login-token'];
+        const loginToken =
+          req.headers['meteor-login-token'] ||
+          req.cookies['meteor-login-token'];
         userContext = await getUserForContext(
           loginToken,
           meteorApolloConfig.userFields
