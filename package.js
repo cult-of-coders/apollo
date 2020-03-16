142d4e19e95b1c0c13e08875143adbea27e6d83c
Theodor Diaconu <diaconu.theodor@gmail.com>
2020-03-16 19:01:37 +0200

    bump to 0.10.0

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "cultofcoders:apollo",
-  version: "0.9.3",
+  version: "0.10.0",
   // Brief, one-line summary of the package.
   summary: "Meteor & Apollo integration",
   // URL to the Git repository containing the source code for this package.
